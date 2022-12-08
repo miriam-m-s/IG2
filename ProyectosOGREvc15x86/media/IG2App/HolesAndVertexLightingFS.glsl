49dbc8b0b9adf49f50bb63fa482a39d22ee6c430
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2022-12-08 18:50:13 +0100

    ejericicio 11

--- ProyectosOGREvc15x86/media/IG2App/HolesAndVertexLightingFS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/HolesAndVertexLightingFS.glsl
@@ -1,21 +1,21 @@
 
 #version 330 core
 uniform sampler2D corrosion; // tipo sampler2D para texturas 2D
-in vec3 vFrontColor; // color de la iluminación interpolado
-in vec3 vBackColor; // color de la iluminación interpolado
+in vec3 vFrontColor; // color de la iluminaciï¿½n interpolado
+in vec3 vBackColor; // color de la iluminaciï¿½n interpolado
 uniform float BF; // blending factor
 uniform float intLuzAmb; // luz ambiente blanca
 in vec2 vUv0; // out del vertex shader
 out vec4 fFragColor;
 
 void main(void) {
-		vec3 colorCelda = vec3(texture(corrosion, vUv0)); // acceso a téxel
+		vec3 colorCelda = vec3(texture(corrosion, vUv0)); // acceso a tï¿½xel
 		vec3 color;
 
 		if(colorCelda.x < 0.6){
 			
-			if (gl_FrontFacing) color = vFrontColor * color;
-			else color = vBackColor * color;
+			if (gl_FrontFacing) color = vFrontColor;
+			else color = vBackColor;
 
 			fFragColor = vec4(color*intLuzAmb, 1.0); // out
 		}
--- ProyectosOGREvc15x86/media/IG2App/HolesAndVertexLightingVS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/HolesAndVertexLightingVS.glsl
@@ -3,21 +3,21 @@ in vec2 uv0;
 in vec4 vertex;
 in vec3 normal;
 out vec2 vUv0;
-uniform float3 frontMaterial;
-uniform float3 backMaterial;
-uniform float3 lightDiffuse;
-uniform vec3 lightPosition;
+out vec3 vFrontColor; // color de la iluminaciï¿½n interpolado
+out vec3 vBackColor; // color de la iluminaciï¿½n interpolado
+uniform vec3 frontMaterial;
+uniform vec3 backMaterial;
+uniform vec3 lightDiffuse;
+uniform vec4 lightPosition;
 uniform mat4 normalMat;
-out vec3 vFrontColor; // color de la iluminación interpolado
-out vec3 vBackColor; // color de la iluminación interpolado
 uniform mat4 modelViewProjMat;
 
 float diff(vec3 cVertex, vec3 cNormal)
 {
-		vec3 lightDir = lightPosition.xyz; // directional light
-		if (lightPosition.w == 1) // positional light
+	vec3 lightDir = lightPosition.xyz; // directional light
+	if (lightPosition.w == 1) // positional light
 		lightDir = lightPosition.xyz - cVertex;
-		return max(dot(cNormal, normalize(lightDir)), 0.0);
+	return max(dot(cNormal, normalize(lightDir)), 0.0);
 }
 
 void main() {
@@ -25,7 +25,7 @@ void main() {
 	vec3 viewVertex = vec3(modelViewProjMat * vertex);
 	vec3 viewNormal = normalize(vec3(normalMat * vec4(normal,0)));
 
-	vec3 diffuse = diff(viewVertex, viewNormal) *lightDiffuse * frontMaterial;
+	vec3 diffuse = diff(viewVertex, viewNormal) * lightDiffuse * frontMaterial;
 	vFrontColor = diffuse; // + specular
 
 	diffuse = diff(viewVertex, -viewNormal) * lightDiffuse * backMaterial;
